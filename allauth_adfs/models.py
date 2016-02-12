bbf63d555deda8bc630ebb9e6a60063a9b33d198
gordon <wgordonw1@gmail.com>
2016-02-12 15:41:23 -0500

    move signal listener to models so it is always imported

---
+++ allauth_adfs/models.py
@@ -0,0 +1,20 @@
+from allauth.account.signals import user_logged_in
+from allauth.socialaccount.adapter import DefaultSocialAccountAdapter, get_adapter
+from allauth.socialaccount.providers import registry
+from django.contrib import messages
+from django.dispatch import receiver
+from .socialaccount.providers.adfs_oauth2.provider import ADFSOAuth2Provider
+from .socialaccount.adapter import SocialAccountAdapter
+
+@receiver(user_logged_in)
+def ensure_staff_login_via_adfs(**kwargs):
+    adapter = get_adapter()
+    if isinstance(adapter, SocialAccountAdapter):
+        sociallogin = kwargs.get("sociallogin")
+        via_adfs = sociallogin and sociallogin.account.provider == ADFSOAuth2Provider.id
+        if not via_adfs:
+            changed, user = adapter.update_user_fields(user=kwargs["user"])
+            if changed:
+                user.save()
+                provider = registry.by_id(ADFSOAuth2Provider.id)
+                messages.warning(kwargs["request"], 'User account modified due to log in provider. Log in with the %s provider to restore functionality when needed.' % provider.name)
--- allauth_adfs/socialaccount/adapter.py
+++ allauth_adfs/socialaccount/adapter.py
@@ -46,16 +46,3 @@ class SocialAccountAdapter(DefaultSocialAccountAdapter):
                     changed = True
         
         return changed, user
-
-@receiver(user_logged_in)
-def ensure_staff_login_via_adfs(**kwargs):
-    adapter = get_adapter()
-    if isinstance(adapter, SocialAccountAdapter):
-        sociallogin = kwargs.get("sociallogin")
-        via_adfs = sociallogin and sociallogin.account.provider == ADFSOAuth2Provider.id
-        if not via_adfs:
-            changed, user = adapter.update_user_fields(kwargs["user"])
-            if changed:
-                user.save()
-                provider = registry.by_id(ADFSOAuth2Provider.id)
-                messages.warning(kwargs["request"], 'User account modified due to log in provider. Log in with the %s provider to restore functionality when needed.' % provider.name)
