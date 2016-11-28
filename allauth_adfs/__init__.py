3df66eaf16b61ad05e7ee4bfe29f1190af4d485c
Gordon Pendleton <gordon@internal.gandssecurityincorporated.com>
2016-11-28 13:46:09 -0500

    Revert "use app ready to import the adfs provider"
    This reverts commit 87e77b1ef6dc1dbea81f14cf0bb69110cabc62a2.

--- allauth_adfs/__init__.py
+++
@@ -1,1 +0,0 @@
-default_app_config = 'allauth_adfs.apps.DefaultConfig'
--- allauth_adfs/apps.py
+++
@@ -1,8 +0,0 @@
-from django.apps import AppConfig, apps
-
-
-class DefaultConfig(AppConfig):
-    name = "allauth_adfs"
-
-    def ready(self):
-        from .socialaccount.providers.adfs_oauth2.provider import ADFSOAuth2Provider
