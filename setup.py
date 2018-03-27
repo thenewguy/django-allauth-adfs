5447e46313efd0c73b6a0686b2296109c72f88dd
gordon pendleton <gordon@internal.gandssecurityincorporated.com>
2018-03-27 16:14:44 -0400

    bump version

--- setup.py
+++ setup.py
@@ -72,7 +72,7 @@ if version_info < (3, 3):
 
 setup(
     name = "django-allauth-adfs",
-    version = "0.0.4",
+    version = "0.0.5",
     author = "gordon",
     author_email = "wgordonw1@gmail.com",
     description = "ADFS oAuth provider for django-allauth",
