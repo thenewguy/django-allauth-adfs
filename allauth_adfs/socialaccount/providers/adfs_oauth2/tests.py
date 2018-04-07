7484bbd3020e2268cd1e010c5b90ead92eb65cd7
gordon <wgordonw1@gmail.com>
2018-04-06 21:45:27 -0400

    whitespace

--- allauth_adfs/socialaccount/providers/adfs_oauth2/tests.py
+++ allauth_adfs/socialaccount/providers/adfs_oauth2/tests.py
@@ -28,24 +28,24 @@ def encode(source):
 class TestProviderUrls(TestCase):
     def test_urls_importable(self):
         from allauth_adfs.socialaccount.providers.adfs_oauth2 import urls
-    
+
     def test_urls_populated(self):
         from allauth_adfs.socialaccount.providers.adfs_oauth2 import urls
         self.assertIsInstance(urls.urlpatterns, list)
         self.assertTrue(urls.urlpatterns)
-        
+
     def test_login_url(self):
         registry = providers.ProviderRegistry()
         registry.load()
         provider = registry.by_id(ADFSOAuth2Provider.id)
         login_url = provider.get_login_url(request=None)
         self.assertEquals(login_url, "/accounts/adfs_oauth2/login/")
-    
+
     def test_template_login_url(self):
         registry = providers.ProviderRegistry()
         registry.load()
         provider = registry.by_id(ADFSOAuth2Provider.id)
-        
+
         factory = RequestFactory()
         request = factory.get('/accounts/login/')
         c = RequestContext(request, {
@@ -56,7 +56,7 @@ class TestProviderUrls(TestCase):
             {% provider_login_url provider.id %}
         """)
         content = t.render(c).strip()
-        
+
         self.assertEquals(content, "/accounts/adfs_oauth2/login/")
 
 
@@ -77,7 +77,7 @@ class UtilsTests(TestCase):
         data = {"guid": "2brp/e0eREqX7SzEA6JjJA=="}
         uid = default_extract_uid_handler(data, None)
         self.assertEquals(uid, six.text_type('fde9bad9-1eed-4a44-97ed-2cc403a26324'))
-        
+
 
 
 class ADFSTests(OAuth2TestsMixin, TestCase):
@@ -88,52 +88,52 @@ class ADFSTests(OAuth2TestsMixin, TestCase):
         "first_name": "jane",
         "last_name": "doe"
     }
-    
+
     def get_mocked_response(self):
         return MockedResponse(200, '')
-    
+
     def get_login_response_json(self, **kwargs):
         jwt = self.get_dummy_jwt()
         return '{"access_token":"%s"}' % jwt
-    
+
     def test_unencrypted_token_payload(self):
         jwt = self.get_dummy_jwt()
-        
+
         encoded_claims_json = parse_token_payload_segment(jwt)
         decoded_claims_json = decode_payload_segment(encoded_claims_json)
         parsed_claims = json.loads(decoded_claims_json)
-        
+
         claims = self.default_claims
-        
+
         self.assertEqual(claims["guid"], parsed_claims["guid"])
         self.assertEqual(claims["upn"], parsed_claims["upn"])
         self.assertEqual(claims["first_name"], parsed_claims["first_name"])
         self.assertEqual(claims["last_name"], parsed_claims["last_name"])
-    
+
     @unittest.skip("refresh tokens are not supported")
     def test_account_refresh_token_saved_next_login(self, **kwargs):
         pass
-    
+
     @unittest.skip("cannot match expected token value")
     def test_account_tokens(self, **kwargs):
         pass
-    
+
     def get_dummy_jwt(self, claims=None):
         if claims is None:
             claims = self.default_claims
-        
+
         # raw data
         header = {
             "alg": "none",
             "typ":"JWT"
         }
-        
+
         signature = ""
-        
+
         # payload data
         header_data = encode(json.dumps(header))
         claims_data = encode(json.dumps(claims))
         signature_data = encode(signature)
         payload = [header_data, claims_data, signature_data]
-        
+
         return ".".join(payload)
