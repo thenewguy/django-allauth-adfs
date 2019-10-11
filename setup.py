5f95c70ce0df548093993fcb49f5973baf3826e6
gordon <wgordonw1@gmail.com>
2019-10-11 12:58:28 -0400

    update version number

--- setup.py
+++ setup.py
@@ -74,7 +74,7 @@ if version_info < (3, 3):
 
 setup(
     name = "django-allauth-adfs",
-    version = "0.1.4",
+    version = "0.1.5",
     author = "gordon",
     author_email = "wgordonw1@gmail.com",
     description = "ADFS oAuth provider for django-allauth",
