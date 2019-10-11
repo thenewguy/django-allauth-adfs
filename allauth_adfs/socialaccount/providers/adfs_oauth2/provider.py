b2cf0f901e6eda7558eae0a8b2c7a2a67d1887e9
gordon <wgordonw1@gmail.com>
2019-10-11 17:55:39 -0400

    Revert "whitespace"
    This reverts commit 2e5746f0edc4a26113e33a0b6334781c3a6ba7bc.

--- allauth_adfs/socialaccount/providers/adfs_oauth2/provider.py
+++ allauth_adfs/socialaccount/providers/adfs_oauth2/provider.py
@@ -13,17 +13,17 @@ class ADFSOAuth2Provider(OAuth2Provider):
     name = 'ADFS Oauth2'
     package = 'allauth_adfs.socialaccount.providers.adfs_oauth2'
     account_class = ADFSOAuth2Account
-
+    
     def get_auth_params(self, request, action):
         params = super(ADFSOAuth2Provider, self).get_auth_params(request, action)
         if "resource" not in params:
             raise ImproperlyConfigured("'resource' must be supplied as a key of the AUTH_PARAMS dict under adfs_oauth2 in the SOCIALACCOUNT_PROVIDERS setting.")
         return params
-
+    
     def extract_extra_data(self, data):
         app = self.get_app(self.request)
         return self.get_settings().get("extract_extra_data_handler", default_extract_extra_data_handler)(data, app)
-
+    
     def extract_uid(self, data):
         app = self.get_app(self.request)
         return self.get_settings().get("extract_uid_handler", default_extract_uid_handler)(data, app)
@@ -31,7 +31,7 @@ class ADFSOAuth2Provider(OAuth2Provider):
     def extract_common_fields(self, data):
         app = self.get_app(self.request)
         return self.get_settings().get("extract_common_fields_handler", default_extract_common_fields_handler)(data, app)
-
+    
     def extract_email_addresses(self, data):
         app = self.get_app(self.request)
         return self.get_settings().get("extract_email_addresses_handler", default_extract_email_addresses_handler)(data, app)
