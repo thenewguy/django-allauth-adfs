65945a9847001874879c4535b000103f8a889f2e
Gordon Pendleton <wgordonw1@gmail.com>
2020-05-19 21:16:36 -0400

    removed newline in log message

--- allauth_adfs/socialaccount/providers/adfs_oauth2/views.py
+++ allauth_adfs/socialaccount/providers/adfs_oauth2/views.py
@@ -166,7 +166,7 @@ class ADFSOAuth2Adapter(OAuth2Adapter):
                     invalid_payload = 'Failed to decode for inspection.'
                     logger.exception("Unable to decode token from %s for inspection!", self.host)
                 logger.exception(
-                    "Invalid token encountered from %s. We expected an audience value of '%s' and allowed for %s seconds of clock drift. The token data is displayed for debugging purposes and must not otherwise be trusted:\n%s",
+                    "Invalid token encountered from %s. We expected an audience value of '%s' and allowed for %s seconds of clock drift. The token data is displayed for debugging purposes and must not otherwise be trusted: %s",
                     self.host,
                     kwargs["audience"],
                     kwargs["leeway"],
